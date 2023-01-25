03cd1c6f179ffb2baa00b82fa169dd2059c701a1
Zakaria-e <zakaria1248@gmail.com>
2023-01-24 19:34:39 -0500

    new file:   0.EverythingProgram/main.cpp new file:   0.EverythingProgram/main.cppbak new file:   2.EnvironmentSetup/1.Windows/8.me do/main.cpp new file:   3.FirstSteps/WhereYouLive/main.cpp modified:   0.EverythingProgram/main.cpp

---
+++ 3.FirstSteps/WhereYouLive/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    std::string endz;
+    std::cout << "Which endz you from fam!\n";// << std::endl;
+    std::cin >> endz;
+    std::cout << "I've heard great things about "<< endz << ". I'd like to go there sometime." << std::endl;
+
+    return 0;
+}
